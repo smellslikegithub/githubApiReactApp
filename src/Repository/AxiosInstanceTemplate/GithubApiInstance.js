552c4c4831c1655e09cc7016a78e55da70763a59
Ashish Husain <ashish.m.husain@gmail.com>
2020-11-30 20:20:37 +0100

    Feature for search done, updated the git ignore too

--- src/Repository/AxiosInstanceTemplate/GithubApiInstance.js
+++ src/Repository/AxiosInstanceTemplate/GithubApiInstance.js
@@ -1,10 +1,31 @@
 import axios from "axios";
 
-const GitHubInstance = axios.create({
-    // format for url with arguments is "https://url?queryparams"
-    baseURL: `https://api.github.com/users?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`,
-    method: "get",
-    responseType: "json"
-});
 
-export default GitHubInstance;
+class GithubApiInstance {
+
+    baseUrlLink = () => {
+        const GitHubInstance = axios.create({
+            // format for url with arguments is "https://url?queryparams"
+            baseURL: `https://api.github.com/users?client_id=
+            ${process.env.REACT_APP_CLIENT_ID}&client_secret=
+            ${process.env.REACT_APP_CLIENT_SECRET}`,
+            method: "get",
+            responseType: "json"
+        });
+        return GitHubInstance;
+    }
+
+    searchUser(param) {
+        const getUser = axios.create({
+            baseURL: `https://api.github.com/search/users?q=${param}&
+            client_id=${process.env.REACT_APP_CLIENT_ID}&
+            client_secret=${process.env.REACT_APP_CLIENT_SECRET}`,
+            method: "get",
+            responseType: "json"
+        });
+
+        return getUser;
+    }
+}
+
+export default GithubApiInstance;
